5486e1226d992be02261bdb64c04f838bf3335aa
mark-torres10 <markptorres1@gmail.com>
2025-05-27 16:33:06 -0400

    Complete adding blogposts

--- data/samples/website/blogposts.py
+++ data/samples/website/blogposts.py
@@ -1,198 +1,4 @@
-"""Samples pulled from the website itself."""
-
-copy = [
-    {
-        "metadata": {
-            "source": "https://www.frozenfields.live/",
-            "type": "website",
-            "subtype": "slogan"
-        },
-        "text": "Find your feel good"
-    },
-    {
-        "metadata": {
-            "source": "https://www.frozenfields.live/",
-            "type": "website",
-            "subtype": "product"
-        },
-        "text": """
-        At Frozen Fields we pride ourselves in our passion for the hemp plant
-        and pushing the boundaries of how it can improve our daily lives.
-
-        Discovered by our lead scientist-Frozen Field's patent-pending
-        'Formula-X' is an extremely bioavailable form of hemp-derived THC;
-        with an almost-instantaneous, yet long-lasting effect. 
-
-        Try it out today & feel the difference!
-        """
-    },
-    {
-        "metadata": {
-            "source": "https://www.frozenfields.live/",
-            "type": "website",
-            "subtype": "product"
-        },
-        "text": """
-        Crafted with fresh-frozen, full-spectrum live resin to ensure the
-        perfect entourage effect-our gummies provide a controlled & consistent
-        dosage of cannabinoids every time - meaning they're just as easy to
-        enjoy as they are to eat!
-
-        Gluten-free, non-GMO, and vegan... but most importantly-delicious.
-        """
-    },
-    {
-        "metadata": {
-            "source": "https://www.frozenfields.live/vapes/",
-            "type": "website",
-            "subtype": "product"
-        },
-        "text": """
-        Formula X:
-        Our brand new, extremely bio-available form of hemp-derived Delta-8 THC
-        discovered by the team at Frozen Fields. This proprietary process makes
-        Formula-X experiences vivid and more euphoric than anything previously
-        seen in the alternative cannabis category. Frozen Fields' vapes have the
-        extra protection of being in child-resistant packaging by LeafLocker®
-        that is made in the USA with wind energy.
-        """
-    },
-    {
-        "metadata": {
-            "source": "https://www.frozenfields.live/vapes/",
-            "type": "website",
-            "subtype": "product"
-        },
-        "text": """
-        THC-A:
-        If you're a cannabis enthusiast, you've probably heard of THC, the
-        compound responsible for the plant's psychoactive effects. But have you
-        ever explored the astounding benefits held within its precursor, THCA?
-        THCA cannabis, often overlooked, has been gaining attention for its
-        potential therapeutic advantages. Frozen Fields' vapes have the extra
-        protection of being in child-resistant packaging by LeafLocker® that is
-        made in the USA with wind energy.
-        """
-    },
-    {
-        "metadata": {
-            "source": "https://www.frozenfields.live/vapes/",
-            "type": "website",
-            "subtype": "product"
-        },
-        "text": """
-        Delta-8:
-        We want to give our customers the best Delta 8 live resin vape with
-        maximum potency. We create our live resin with a specialized cold
-        extraction method that preserves the full spectrum of compounds present
-        in hemp flower, offering improved terpene preservation, enhanced
-        cannabinoid content, full-spectrum extracts, superior flavor and aroma,
-        and potential therapeutic benefits. This synergistic combination of
-        compounds contributes to the entourage effect.
-        """
-    },
-    {
-        "metadata": {
-            "source": "https://www.frozenfields.live/vapes/",
-            "type": "website",
-            "subtype": "product"
-        },
-        "text": """
-        Our strain profiles:
-
-        Sativa:
-        This is our fast-acting and uplifting blend. Our patent-pending blend
-        delivers a profound sense of energy - a friendly blast of get up and go -
-        before you settle back into a richly rewarding experience. This is the
-        Sativa effect, but with the unique fuller, faster, feel-good factor of
-        Formula-X.
-
-        Indica:
-        Our Indica blend has a euphoric onset followed by complete relaxation.
-        You get the feel-good factor as you experience exhilaration before
-        settling into the sublime state you would expect from a true Indica.
-
-        Hybrid:
-        This is a remix of our Indica and Sativa. A true Hybrid with the
-        fast-acting and uplifting effects of our Sativa blend and the relaxation
-        and euphoric nature of our Indica blend, creating a well-rounded
-        experience for someone looking for a go-to product for all-day use.
-        """
-    },
-    {
-        "metadata": {
-            "source": "https://www.frozenfields.live/gummies/",
-            "type": "website",
-            "subtype": "product"
-        },
-        "text": """
-        FORMULA-X THC:
-        150mg (15mg/gummy)
-
-        Our Formula-X gummies combine the potent effects of Formula-X &
-        hemp-derived THC with CBD live resin for a balanced and controlled
-        effect. We believe edibles should feel AND taste good; our gummies are
-        bursting with fruit flavor - with no taste of cannabinoids. Feel good,
-        not guilty; all our gummies are Farm Bill compliant with less than 0.3%
-        Delta-9 THC concentration on a dry weight basis, verified by 3rd-party
-        testing.
-        """
-    },
-    {
-        "metadata": {
-            "source": "https://www.frozenfields.live/gummies/",
-            "type": "website",
-            "subtype": "product"
-        },
-        "text": """
-        DELTA-8 THC:
-        250 or 500mg (50mg/gummy)
-
-        Prefer the Delta-8 experience? 
-
-        Sour watermelon, tangy blood orange, and sweet-and-sour lemon-lime -
-        our Delta-8 gummies combine all the flavor possibilities of our
-        Formula-X gummies with 50mg of D8 per gummy for a tasty & potent
-        experience. Available in 5- or 10-count bags; find your own 'feel good'
-        or share the fun with friends.
-        """
-    },
-    {
-        "metadata": {
-            "source": "https://www.frozenfields.live/company/",
-            "type": "website",
-            "subtype": "company"
-        },
-        "text": """
-        It all starts FROZEN AT -200°
-        Grown on the 45th parallel
-
-        OUR COMPANY
-        Frozen Fields was established in 2022 to bring innovation and quality
-        to the hemp-derived cannabis space, with the goal of providing the most
-        effective and enjoyable products on the market. Our team consists of
-        cannabis industry veterans, an industry-leading scientist, and a proven
-        multi-million dollar CPG sales team. 
-
-        We're focused on creating unique formulations - using only quality
-        ingredients - to provide consumers with experiences they can love and
-        rely on.
-
-        OUR VALUES
-        As cannabis cultivators, consumers, and advocates, we believe in the
-        availability of quality products at affordable prices. Every Frozen
-        Fields' product is part art, part science; grounded in sustainability
-        and showcasing the natural benefits of the hemp plant. Our expertise
-        and innovation in the science of cannabinoids allows us to remain
-        adaptable in an ever-changing market.  
-
-        There are plenty of hemp products to choose from; Frozen Fields aims
-        to create unmatched user experiences that make the choice easy. We're
-        sure customers will enjoy the fresher, fuller, more flavorsome experience of Frozen Fields. 
-
-        Let us help you find your feel good!
-        """
-    },
+blogposts = [
     {
         "metadata": {
             "source": "https://www.frozenfields.live/blog/",
@@ -251,7 +57,8 @@ copy = [
         Remember, this is just a quick summary. The legal world can be complex,
         but we hope this helps you understand what's happening in the hemp
         industry!
-        """
+        """,
+        "timestamp": "2024-11-21"
     },
     {
         "metadata": {
@@ -297,7 +104,8 @@ copy = [
         crackdown underscores the state's dedication to adapt and fortify
         regulatory frameworks in response to evolving market dynamics and
         emerging challenges.
-        """
+        """,
+        "timestamp": "2024-11-10"
     },
     {
         "metadata": {
@@ -476,4 +284,49 @@ copy = [
         """,
         "timestamp": "2023-08-04"
     },
+    {
+        "metadata": {
+            "source": "https://www.frozenfields.live/time-is-now-to-get-heavy-on-compliance-for-the-looming-crackdown/",
+            "type": "website",
+            "subtype": "blog"
+        },
+        "text": """
+        Time is now to get heavy on compliance for the looming crackdown.
+
+        Product safety and compliance aren't very strong in our industry, they
+        are however a cornerstone of our operations - we're all about ensuring
+        the products reaching your customers meet the highest standards.
+
+        Wonder why major brands like Pax, who have hundreds of millions at stake,
+        entrust us to manufacture and master distribute their Delta 8 THC
+        products? The answer is simple: we don't cut corners. In fact, we
+        wholeheartedly embrace stringent safety measures, which reflects in our
+        robust, end-to-end control of the entire production process.
+
+        Ever Wondered why C Store Chains trust Hempsi and Frozen Fields as
+        brands they can carry without repercussions? It's because we do things
+        right! If you've been keeping up with our 'How it's Made' email series,
+        you're well aware that we oversee the entire lifecycle of our products -
+        right from the seed to the safety seal.
+
+        Our operations are centralized in our extensive 38,000 Sq Ft lab and
+        fulfillment center, nestled just outside Asheville in North Carolina.
+        This facility is not only ISO 9001 and cGMP certified, but also
+        committed to using the purest grade ingredients, including Pegasus Delta
+        8 THC.
+
+        Every aspect of our production - extraction, conversion, formulation,
+        and even testing - is controlled by us. Our on-site testing lab ensures
+        the quality and safety of our products, backing them up with True Batch
+        Tested COAs from 3rd party labs on completion.
+
+        By offering pre-tested inputs, ingredient tracking, and fulfillment
+        services under one roof, we aim to create a seamless, trustworthy
+        experience for you. Consider partnering with us as future-proofing your
+        business, equipping you to navigate any storm that may brew in the
+        industry with unwavering confidence. Together, let's champion safety
+        and quality in all we do!
+        """,
+        "timestamp": "2023-07-13"
+    }
 ]
